c46e4b120be7f50a55ce6a1d515840099a877b2a
Ashw1n-Khasa <125382465+Ashw1n-Khasa@users.noreply.github.com>
2024-01-20 22:08:33 +0530

    Trying basic array declaration

---
+++ array.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a=10;
+    char c='k';
+    char ak[100]="ashwin";
+    int arr[]={2,5,1,8,0};
+
+    cout<<ak[1]<<endl;//s
+    cout<<arr[3]<<endl;
+    cout<<a<<endl;
+    cout<<c<<endl;
+
+    char str[100];
+    cout<<"Enter string. ";
+    cin>>str;
+
+    cout<<"you entered "<<str<<endl;
+    return  0;
+}
