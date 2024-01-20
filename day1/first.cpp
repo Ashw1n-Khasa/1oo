69180da97dc7f249c4a08e7f6fe6aba6bbdea971
Ashw1n-Khasa <125382465+Ashw1n-Khasa@users.noreply.github.com>
2024-01-20 21:56:41 +0530

    Add files via upload

---
+++ day1/first.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+//library 
+using namespace std;
+//or use "std::cout"
+
+int main(){
+    cout<<"Hi , My name is Ashwin!"<<endl;
+    //cout means show output.
+    //endl means end of line
+
+    int a=10; //declaring var ;
+
+    //datatype varname = value ;
+    //datatypes int , float , char , auto etc
+    cout<<"Value of a : "<<a<<endl;
+
+    int b;
+
+    cout<<"Enter any number :";
+    cin>>b; //taking input in var b with cin
+
+    cout<<"You entered :"<<b<<endl;
+    
+    return 0;
+
+}
